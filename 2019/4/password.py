c28d66c10281cf9df27e70e174954099cf190849
Silviu <silviu.iordache92@gmail.com>
2019-12-04 21:30:44 +0200

    Part 2.

--- 2019/4/password.py
+++ 2019/4/password.py
@@ -1,35 +1,48 @@
-passwords = range(138241, 674034 + 1)
+passwords = range(138241, 674035)
+
+arr = range(1, 5)
+print(arr[3])
+
 
 def test_number(number):
-    arr = [char for char in str(number)]
-
+    arr = [int(x) for x in str(number)]
     last_digit = -1
     curr_digit = -1
-
     increasing_digit_order = True
     dual_found = False
+    secured = False
     dual_digit = -1
-    occurrence = [-1, 0]
     for i in range(len(arr)):
         if i == 0:
             last_digit = arr[i]
         else:
             last_digit = arr[i-1]
             curr_digit = arr[i]
-            if int(curr_digit) - int(last_digit) < 0:
+            if curr_digit - last_digit < 0:
                 increasing_digit_order = False
-        if last_digit == curr_digit:
-            if dual_digit != -1 and dual_digit == curr_digit:
-                dual_digit = -1
+        if last_digit == curr_digit and not secured:
+            if dual_digit == curr_digit:
+                dual_found = False
             else:
-                dual_digit = int(curr_digit)
-    if dual_digit >= 0:
-        dual_found = True
+                dual_digit = curr_digit
+                dual_found = True
+        else:
+            if dual_found:
+                secured = True
+            else:
+                dual_digit = -1
     return dual_found and increasing_digit_order
 
 
+print(test_number(112233))
 print(test_number(123444))
+print(test_number(111122))
+print(test_number(112333))
 
-# print(len(list(filter(lambda x: test_number(x), passwords))))
+# test_arr = [112233, 111222, 333455, 444444, 555678, 112233, 112333]
+# print(list(filter(lambda x: test_number(x), test_arr)))
+
+print(len(list(filter(lambda x: test_number(x), passwords))))
+# print(list(filter(lambda x: test_number(x), passwords)))
 
 
