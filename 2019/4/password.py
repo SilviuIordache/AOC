2178502673457e147a6d2777eebce87a08e3377b
Silviu Iordache <silviu.iordache@caphyon.com>
2019-12-04 18:35:02 +0200

    Add second problem.

--- 2019/4/password.py
+++ 2019/4/password.py
@@ -1,14 +1,35 @@
-passwords = []
+passwords = range(138241, 674034 + 1)
 
-start = 138241
-end = 674034
+def test_number(number):
+    arr = [char for char in str(number)]
 
-passwords = range(start, end + 1)
+    last_digit = -1
+    curr_digit = -1
 
-print(passwords[0])
-print(passwords[end - start])
+    increasing_digit_order = True
+    dual_found = False
+    dual_digit = -1
+    occurrence = [-1, 0]
+    for i in range(len(arr)):
+        if i == 0:
+            last_digit = arr[i]
+        else:
+            last_digit = arr[i-1]
+            curr_digit = arr[i]
+            if int(curr_digit) - int(last_digit) < 0:
+                increasing_digit_order = False
+        if last_digit == curr_digit:
+            if dual_digit != -1 and dual_digit == curr_digit:
+                dual_digit = -1
+            else:
+                dual_digit = int(curr_digit)
+    if dual_digit >= 0:
+        dual_found = True
+    return dual_found and increasing_digit_order
 
-for item in passwords:
 
-def hasTwoEqAdjDigits(number):
+print(test_number(123444))
+
+# print(len(list(filter(lambda x: test_number(x), passwords))))
+
 
