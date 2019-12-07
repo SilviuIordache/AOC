9949cba1910693307ff2616ca58ffe435625ede6
Silviu <silviu.iordache92@gmail.com>
2019-12-07 12:26:30 +0200

    Progress on d7p2.

--- 2019/7/circuit2.js
+++ 2019/7/circuit2.js
@@ -10,7 +10,7 @@ const rl = readline.createInterface({
 });
 
 rl.on('line', function(line) {
-  getHighestSignal(line)
+  //getHighestSignal(line)
 })
 
 
@@ -93,14 +93,14 @@ let getInstructionData = function(number) {
 }
 
 let getPermutations = function() {
-  cmb = Combinatorics.permutation([0, 1, 2, 3, 4]);
+  cmb = Combinatorics.permutation([5, 6, 7, 8, 9]);
   return cmb.toArray();
 }
 
 
-let amplifySignal= function (signal, phaseSetting) {
+let amplifySignal= function (signal, module1Input, phaseSetting) {
 
-  module1 = calculator(signal, phaseSetting[0], 0)
+  module1 = calculator(signal, phaseSetting[0], module1Input)
   module2 = calculator(signal, phaseSetting[1], module1)
   module3 = calculator(signal, phaseSetting[2], module2)
   module4 = calculator(signal, phaseSetting[3], module3)
@@ -116,22 +116,25 @@ let getHighestSignal = function(signal) {
 
   for (let i = 0 ; i < phases.length; i++) {
     let currPhase = phases[i];
-    let val = amplifySignal(signal, currPhase);
-    if(val > maxSignal) {
-      maxSignal = val;
-      maxSignalPerm = phases[i]
+    let val = amplifySignal(signal, 0, currPhase);
+
+    while (val) {
+      if(val > maxSignal) {
+        maxSignal = val;
+        maxSignalPerm = phases[i]
+      }
+      val = amplifySignal(signal, val, currPhase);
     }
+    
   }
   
   console.log(maxSignal, maxSignalPerm);
 }
 
 
-let signal1 = '3,15,3,16,1002,16,10,16,1,16,15,15,4,15,99,0,0';
-let signal2 = '3,23,3,24,1002,24,10,24,1002,23,-1,23,101,5,23,23,1,24,23,23,4,23,99,0,0';
-let signal3 = '3,31,3,32,1002,32,10,32,1001,31,-2,31,1007,31,0,33,1002,33,7,33,1,33,31,31,1,32,31,31,4,31,99,0,0,0';
+let signal1 = '3,26,1001,26,-4,26,3,27,1002,27,2,27,1,27,26,27,4,27,1001,28,-1,28,1005,28,6,99,0,0,5';
+let signal2 = '3,52,1001,52,-5,52,3,53,1,52,56,54,1007,54,5,55,1005,55,26,1001,54,-5,54,1105,1,12,1,53,54,53,1008,54,0,55,1001,55,1,55,2,53,55,53,4,53,1001,56,-1,56,1005,56,6,99,0,0,0,0,10'
 
-
-// getHighestSignal(signal1)
-// getHighestSignal(signal2)
+getHighestSignal(signal1)
+//getHighestSignal(signal2)
 // getHighestSignal(signal3)
